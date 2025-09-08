42589b9df5f1a2ef13649a0f986027e8f975d679
Diego <diegodecstraid@gmail.com>
2025-09-08 00:22:19 -0300

    Introduzindo modelo Lambda e modelando dados

--- screenmatch/src/main/java/alura/java/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/alura/java/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,12 @@
 package alura.java.screenmatch;
 
-import alura.java.screenmatch.model.DadosSerie;
-import alura.java.screenmatch.service.ConsumoApi;
-import alura.java.screenmatch.service.ConverteDados;
+import alura.java.screenmatch.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner{
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
@@ -15,14 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner{
 
     @Override
     public void run (String... args) throws Exception {
-        var consumoApi = new ConsumoApi();
-        var json =  consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=6585022c")
-//        System.out.println(json);
-//        json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-        System.out.println(json);
-        ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dados);
+        Principal principal = new Principal();
+        principal.exibeMenu();
     }
-
 }
